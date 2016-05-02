6b5ba9bf5a8e282ef5c42cf344245e0c9e72846a
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2016-05-02 20:53:14 +0200

    Assign prompt from script

--- DevelopmentConsole.cs
+++ DevelopmentConsole.cs
@@ -1,10 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
 
 namespace Assets.Extensions.DevelopmentConsole {
 
     public class DevelopmentConsole : MonoBehaviour {
 
+        [SerializeField]
+        private InputField commandLine;
 
+        private const string prompt = "> ";
+
+        private void Awake() {
+            Assert.IsNotNull(commandLine);
+
+            commandLine.text = prompt;
+        }
     }
 }
