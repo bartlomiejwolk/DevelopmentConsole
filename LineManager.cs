43b3ffde147c89a3fe8a7d851ae0ed4a9c165d0d
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2016-05-04 23:23:12 +0200

    Extract methods

--- LineManager.cs
+++ LineManager.cs
@@ -11,14 +11,13 @@ namespace Assets.Extensions.DevelopmentConsole {
         [SerializeField]
         private CustomInputField commandLineTemplate;
 
-        private Action returnKeyPressed;
-
         private const string prompt = "> ";
 
+        private Action returnKeyPressed;
         private List<InputField> lines;
+        private InputField activeLine;
 
-        private void Awake()
-        {
+        private void Awake() {
             Assert.IsNotNull(commandLineTemplate);
 
             lines = new List<InputField>();
@@ -40,25 +39,31 @@ namespace Assets.Extensions.DevelopmentConsole {
             }
         }
 
-        private void CreateNewInputLine()
-        {
-            // instantiate
+        // todo rename to CreateNewCmdLine
+        private void CreateNewInputLine() {
+            var cmdLine = InstantiateNewLine();
+            lines.Add(cmdLine);
+            activeLine = cmdLine;
+            SetActiveLineVerticalPosition();
+        }
+
+        private CustomInputField InstantiateNewLine() {
             var cmdLine = Instantiate(commandLineTemplate);
             cmdLine.gameObject.SetActive(true);
             cmdLine.transform.SetParent(commandLineTemplate.transform.parent, false);
+            return cmdLine;
+        }
 
-            lines.Add(cmdLine);
-
-            // set vertical position
-            var cmdLineRect = cmdLine.GetComponent<RectTransform>();
-            if (cmdLineRect != null)
-            {
+        private void SetActiveLineVerticalPosition() {
+            var cmdLineRect = activeLine.GetComponent<RectTransform>();
+            if (cmdLineRect != null) {
                 var verticalPos = CalculatePositionForNewLine();
                 cmdLineRect.anchoredPosition = new Vector2(cmdLineRect.anchoredPosition.x, verticalPos);
             }
         }
 
         private int CalculatePositionForNewLine() {
+            // todo get input field height instead of using hardcoded values
             var verticalOffset = (lines.Count * 30) - 15;
             return -verticalOffset;
         }
