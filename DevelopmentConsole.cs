9bf947cc6c71735efb1646fc2d95be8dd0310905
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2016-05-04 21:06:57 +0200

    Create LineManager

--- DevelopmentConsole.cs
+++ DevelopmentConsole.cs
@@ -1,36 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.Assertions;
 using UnityEngine.UI;
+#pragma warning disable 649
 
 namespace Assets.Extensions.DevelopmentConsole {
 
+    [RequireComponent(typeof(LineManager))]
     public class DevelopmentConsole : MonoBehaviour {
 
         [SerializeField]
-        private CustomInputField commandLine;
-
-        private const string prompt = "> ";
+        private LineManager lineManager;
 
         private void Awake() {
-            Assert.IsNotNull(commandLine);
-
-            commandLine.text = prompt;
         }
 
-        private void Start() {
-            CreateNewInputLine();
-        }
-
-        private void OnEndEdit(string text) {
-            CreateNewInputLine();
-        }
-
-        private void CreateNewInputLine() {
-            var cmdLine = Instantiate(commandLine);
-            cmdLine.gameObject.SetActive(true);
-            cmdLine.transform.SetParent(commandLine.transform.parent, false);
-            cmdLine.onEndEdit.AddListener(OnEndEdit);
-        }
     }
 }
---
+++ LineManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace Assets.Extensions.DevelopmentConsole {
+
+    public class LineManager : MonoBehaviour {
+
+        [SerializeField]
+        private CustomInputField commandLineTemplate;
+
+        private const string prompt = "> ";
+
+        private List<InputField> lines;
+
+        private void Awake()
+        {
+            Assert.IsNotNull(commandLineTemplate);
+
+            commandLineTemplate.text = prompt;
+        }
+
+        private void Start()
+        {
+            CreateNewInputLine();
+        }
+
+        private void OnEndEdit(string text)
+        {
+            CreateNewInputLine();
+        }
+
+        private void CreateNewInputLine()
+        {
+            var cmdLine = Instantiate(commandLineTemplate);
+            cmdLine.gameObject.SetActive(true);
+            cmdLine.transform.SetParent(commandLineTemplate.transform.parent, false);
+            cmdLine.onEndEdit.AddListener(OnEndEdit);
+
+            lines.Add(cmdLine);
+
+            var cmdLineRect = cmdLine.GetComponent<RectTransform>();
+            if (cmdLineRect != null) {
+                var verticalPos = CalculatePositionForNewLine();
+                cmdLineRect.anchoredPosition = new Vector2(cmdLineRect.anchoredPosition.x, verticalPos);
+            }
+        }
+
+        private int CalculatePositionForNewLine() {
+            throw new System.NotImplementedException();
+        }
+
+    }
+
+}
