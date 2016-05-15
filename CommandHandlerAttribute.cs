ff4e4d359d55d4999abd31dbe388dc1dc056d023
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2016-05-15 20:15:52 +0200

    Formatting change

--- CommandHandlerAttribute.cs
+++ CommandHandlerAttribute.cs
@@ -2,8 +2,7 @@
 
 namespace DevelopmentConsoleTool {
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property)]
-    public class CommandHandlerAttribute : Attribute
-    {
+    public class CommandHandlerAttribute : Attribute {
         public string Name { get; set; }
         public string Description { get; set; }
     }
