fe43ae50fe351f93aebc690db03f375bb198c4a1
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2016-05-15 12:42:41 +0200

    Create CommandHandlerAttribute

---
+++ CommandHandlerAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DevelopmentConsole {
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property)]
+    public class CommandHandlerAttribute : Attribute
+    {
+    }
+}
