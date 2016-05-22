75a3217e8004deaeb454cd5d4b1faff023e122c8
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2016-05-22 19:42:56 +0200

    Reorder methods

--- CommandLine.cs
+++ CommandLine.cs
@@ -44,51 +44,16 @@ namespace DevelopmentConsoleTool {
 	    protected override void Start() {
 		    text = prompt;
 		    ActivateInputField();
-		    StartCoroutine(MoveTextEnd_NextFrame());
+		    MoveCaretToEnd();
 	    }
 
 	    #endregion
 
-	    private void RedefineUpDownArrowBehavior() {
-		    var currentEvent = Event.current;
-		    if (currentEvent.keyCode == KeyCode.UpArrow ||
-				currentEvent.keyCode == KeyCode.DownArrow) {
-
-			    currentEvent.Use();
-			    MoveCaretToEnd();
-		    }
+	    public void MoveCaretToEnd() {
+		    StartCoroutine(MoveTextEnd_NextFrame());
 	    }
 
-	    private char ValidateInputHandler(string fieldText, int charIndex, char addedChar)
-        {
-            if (IgnoredChars.Contains(addedChar.ToString())) {
-                return '\0';
-            }
-            return addedChar;
-        }
-
-        private void ValueChangedHandler(string value) {
-            // prevent prompt to be deleted
-            if (value.Length < prompt.Length) {
-                text = prompt;
-            }
-
-            // prevent caret from going onto the prompt
-            if (caretPosition < prompt.Length) {
-                MoveTextEnd(false);
-            }
-        }
-
-	    public void MoveCaretToEnd() {
-            StartCoroutine(MoveTextEnd_NextFrame());
-        }
-
-        IEnumerator MoveTextEnd_NextFrame() {
-            yield return 0; 
-            MoveTextEnd(false);
-        }
-
-		public string GetCommandString()
+	    public string GetCommandString()
 		{
 			var cmdString = text.Substring(prompt.Length);
 			return cmdString;
@@ -110,6 +75,44 @@ namespace DevelopmentConsoleTool {
 			text = result;
 		}
 
-	}
+	    private void RedefineUpDownArrowBehavior() {
+		    var currentEvent = Event.current;
+		    if (currentEvent.keyCode == KeyCode.UpArrow ||
+		        currentEvent.keyCode == KeyCode.DownArrow) {
+
+			    currentEvent.Use();
+			    MoveCaretToEnd();
+		    }
+	    }
+
+	    IEnumerator MoveTextEnd_NextFrame() {
+		    yield return 0; 
+		    MoveTextEnd(false);
+	    }
+
+	    #region EVENT HANDLERS
+
+	    private char ValidateInputHandler(string fieldText, int charIndex, char addedChar)
+	    {
+		    if (IgnoredChars.Contains(addedChar.ToString())) {
+			    return '\0';
+		    }
+		    return addedChar;
+	    }
+
+	    private void ValueChangedHandler(string value) {
+		    // prevent prompt to be deleted
+		    if (value.Length < prompt.Length) {
+			    text = prompt;
+		    }
+
+		    // prevent caret from going onto the prompt
+		    if (caretPosition < prompt.Length) {
+			    MoveTextEnd(false);
+		    }
+	    }
+
+	    #endregion
+    }
 
 }
