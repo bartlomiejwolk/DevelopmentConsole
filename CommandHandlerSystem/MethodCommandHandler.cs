058ce12432b892277d197e14c66cc8610a21a471
Bartlomiej Wolk <bwolk@goodgamestudios.com>
2016-05-23 19:14:39 +0200

    Get method parameters

--- CommandHandlerSystem/MethodCommandHandler.cs
+++ CommandHandlerSystem/MethodCommandHandler.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 
 namespace DevelopmentConsoleTool.CommandHandlerSystem {
 
     public class MethodCommandHandler : CommandHandler {
 
-        private readonly MethodInfo methodInfo;
-        private readonly List<ParamInfo> paramInfos = new List<ParamInfo>();
+        private readonly MethodInfo _methodInfo;
+        private readonly List<ParamInfo> paramInfos;
 
         public MethodCommandHandler(
             Type type,
@@ -16,11 +17,22 @@ namespace DevelopmentConsoleTool.CommandHandlerSystem {
             string commandName,
             string description) : base(type, obj, commandName, description) {
 
-            this.methodInfo = methodInfo;
+            _methodInfo = methodInfo;
+            paramInfos = GetMethodParameters(methodInfo);
+        }
+
+        private List<ParamInfo> GetMethodParameters(MethodInfo methodInfo) {
+            var parameters = methodInfo.GetParameters();
+            var result = new List<ParamInfo>();
+            foreach (var parameter in parameters) {
+                var paramInfo = new ParamInfo(parameter);
+                result.Add(paramInfo);
+            }
+            return result;
         }
 
         public override void Invoke(params string[] arguments) {
-            methodInfo.Invoke(ObjectReference.Target, null);
+            _methodInfo.Invoke(ObjectReference.Target, null);
         }
     }
 }
