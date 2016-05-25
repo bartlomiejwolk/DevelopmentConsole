aebd4ac3892f61c360c78dc69915c01500ddf70b
Bartlomiej Wolk <bwolk@goodgamestudios.com>
2016-05-25 20:36:19 +0200

    Assign option template to CodeCompletion component

--- CodeCompletion.cs
+++ CodeCompletion.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 namespace DevelopmentConsoleTool {
     
-    public class CodeCompletion : MonoBehaviour{
+    public class CodeCompletion : MonoBehaviour {
+
+        [SerializeField]
+        private GameObject _optionTemplate;
 
         public void DisplayResults(List<Match> results) {
             throw new NotImplementedException();
