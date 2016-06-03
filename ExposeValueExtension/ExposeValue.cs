16cd7caf681a1f4c0db240eb04f0a331f73da30e
Bartlomiej Wolk <bwolk@goodgamestudios.com>
2016-06-03 18:54:41 +0200

    Create ExposeValue extension

---
+++ ExposeValueExtension/ExposeValue.cs
@@ -0,0 +1,25 @@
+﻿namespace DevelopmentConsoleTool.ExposeValueExtension {
+    public class ExposeValue {
+
+        private static readonly ExposeValue _instance
+            = new ExposeValue();
+
+        public static ExposeValue Instance {
+            get { return _instance; }
+        }
+
+        private ExposeValue() { }
+
+        public void RegisterValue(string customName, string category, object value) {
+            
+        }
+
+        public void ShowValue(string valueName) {
+            
+        }
+
+        public void HideValue(string valueName) {
+            
+        }
+    }
+}
