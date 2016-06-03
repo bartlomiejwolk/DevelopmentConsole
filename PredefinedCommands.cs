9e890b2430f3146f29e1aacbc3a53e30f044c477
Bartlomiej Wolk <bwolk@goodgamestudios.com>
2016-06-03 19:10:45 +0200

    Create PredefinedCommands script

---
+++ PredefinedCommands.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using DevelopmentConsoleTool.CommandHandlerSystem;
+
+namespace DevelopmentConsoleTool {
+    public class PredefinedCommands : MonoBehaviour {
+
+        private void Start() {
+            CommandHandlerManager.Instance.RegisterCommandHandlers(typeof(CommandHanderTest), this);
+        }
+
+        [CommandHandler]
+        private void ExposeValue(string valueName) {
+            ExposeValueExtension.ExposeValue.Instance.ShowValue(valueName);
+        }
+    }
+}
