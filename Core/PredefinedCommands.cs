d3d3a33ab71559b19880d2ce3597b802287fae2a
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2016-06-05 10:57:57 +0200

    Create ExtendedPredefinedCommands

--- Core/PredefinedCommands.cs
+++ Core/PredefinedCommands.cs
@@ -5,26 +5,14 @@ namespace DevelopmentConsoleTool {
 
     public class PredefinedCommands : MonoBehaviour {
 
-        private void Start() {
-            CommandHandlerManager.Instance.RegisterCommandHandlers(typeof(PredefinedCommands), this);
+        protected virtual void Start() {
+            CommandHandlerManager.Instance.RegisterCommandHandlers(
+				typeof(PredefinedCommands), this);
         }
 
-		// todo create ICommandHandlerInfo with AutoCompleteNames property. Pass it as arg. to CommandHandler attr. ValueExposer could implement this interface.
-        [CommandHandler]
-        private void ExposeValue(string valueName) {
-	        var instance = ValueExposerExtension.ValueExposer.Instance;
-			if (instance != null) {
-				instance.ShowValue(valueName);
-			}
-		}
-
 	    [CommandHandler]
-	    private void HideExposedValue(string valueName) {
-			var instance = ValueExposerExtension.ValueExposer.Instance;
-			if (instance != null)
-			{
-				instance.HideValue(valueName);
-			}
-		}
+	    private void TimeScale(float scale) {
+		    Time.timeScale = scale;
+	    }
     }
 }
---
+++ Extensions/ExtendedPredefinedCommands.cs
@@ -0,0 +1,30 @@
+﻿using DevelopmentConsoleTool.CommandHandlerSystem;
+
+namespace DevelopmentConsoleTool {
+
+	public class ExtendedPredefinedCommands : PredefinedCommands {
+
+		protected override void Start() {
+			base.Start();
+			CommandHandlerManager.Instance.RegisterCommandHandlers(
+				typeof(ExtendedPredefinedCommands), this);
+		}
+
+		// todo create ICommandHandlerInfo with AutoCompleteNames property. Pass it as arg. to CommandHandler attr. ValueExposer could implement this interface.
+		[CommandHandler]
+		private void ExposeValue(string valueName) {
+			var instance = ValueExposerExtension.ValueExposer.Instance;
+			if (instance != null) {
+				instance.ShowValue(valueName);
+			}
+		}
+
+		[CommandHandler]
+		private void HideExposedValue(string valueName) {
+			var instance = ValueExposerExtension.ValueExposer.Instance;
+			if (instance != null) {
+				instance.HideValue(valueName);
+			}
+		}
+	}
+}
