a17c79850713e30087e0bba476acdd990cd452a3
Bartlomiej Wolk <bwolk@goodgamestudios.com>
2016-06-06 19:52:43 +0200

    Create DotSprite prefab

--- Extensions/ValueExposerModule/Extensions/ValueVisualizerModule/GraphDrawer.cs
+++ Extensions/ValueExposerModule/Extensions/ValueVisualizerModule/GraphDrawer.cs
@@ -1,8 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
 
 namespace DevelopmentConsole.Extensions.ValueExposerModule.Extensions.ValueVisualizerModule {
     public class GraphDrawer : MonoBehaviour {
+
+        [SerializeField]
+        private Image _dotSprite;
+
+        private void Awake() {
+            Assert.IsNotNull(_dotSprite);
+        }
+
         public void DrawValuePoint(object value) {
             var number = (float) value;
         }
