83ed1b694deb5db40bf1ce200e50f6fadb6ce4d4
Bartlomiej Wolk <bwolk@goodgamestudios.com>
2016-06-06 18:16:58 +0200

    Create example files

---
+++ Examples/CommandHandlerTest.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using DevelopmentConsole.Core.CommandHandlerSystem;
+
+public class CommandHandlerTest : MonoBehaviour {
+
+	void Start () {
+	    CommandHandlerManager.Instance.RegisterCommandHandlers(typeof(CommandHandlerTest), this);
+	}
+
+    [CommandHandler]
+    private void Echo(string text) {
+        Debug.Log(text);
+    }
+
+    [CommandHandler]
+    private void SetTimescale(float timescale) {
+        Time.timeScale = timescale;
+        Debug.Log("Timescale set to " + Time.timeScale);
+    }
+
+    [CommandHandler]
+    private void Add(int a, int b) {
+        Debug.Log(a + b);
+    }
+
+}
---
+++ Examples/ValueExposerTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using DevelopmentConsole.Extensions.ValueExposerModule.Core;
+
+public class ValueExposerTest : MonoBehaviour {
+
+	void Start () {
+        ExposedValueManager.Instance.RegisterValue("framecount", "cat 1", () => Time.frameCount);
+	    ExposedValueManager.Instance.RegisterValue("timesincelevelload", "cat 1", () => Time.timeSinceLevelLoad);
+        ExposedValueManager.Instance.RegisterValue("string", "cat 2", () => "some string");
+	}
+	
+	void Update () {
+	
+	}
+}
