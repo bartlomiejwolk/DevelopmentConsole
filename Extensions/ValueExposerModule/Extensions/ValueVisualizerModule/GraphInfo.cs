72bfbdf7851ed921117a8223aadb43d56e4c0557
Bartlomiej Wolk <bwolk@goodgamestudios.com>
2016-06-07 19:36:20 +0200

    Extract class to file

---
+++ Extensions/ValueExposerModule/Extensions/ValueVisualizerModule/GraphInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace DevelopmentConsole.Extensions.ValueExposerModule.Extensions.ValueVisualizerModule {
+    public class GraphInfo {
+        public string ValueName { get; set; }
+        public Func<object> ValueDelegate { get; set; } 
+        public GameObject Go { get; set; }
+        public bool Enabled { get; set; }
+
+        public GraphDrawer GraphDrawer {
+            get {
+                if (_graphDrawer == null && Go != null) {
+                    _graphDrawer = Go.GetComponent<GraphDrawer>();
+                }
+                return _graphDrawer;
+            }
+        }
+
+        private GraphDrawer _graphDrawer;
+
+        public void DrawValuePoint() {
+            var value = ValueDelegate();
+            GraphDrawer.DrawValuePoint(value);
+        }
+    }
+}
--- Extensions/ValueExposerModule/Extensions/ValueVisualizerModule/GraphManager.cs
+++ Extensions/ValueExposerModule/Extensions/ValueVisualizerModule/GraphManager.cs
@@ -6,45 +6,22 @@ namespace DevelopmentConsole.Extensions.ValueExposerModule.Extensions.ValueVisua
     
     public class GraphManager {
         
-        private readonly List<ValueGraph> _valueGraphs = new List<ValueGraph>();
+        private readonly List<GraphInfo> _valueGraphs = new List<GraphInfo>();
 
-        public List<ValueGraph> ValueGraphs {
+        public List<GraphInfo> ValueGraphs {
             get { return _valueGraphs; }
         }
 
-        public ValueGraph AddGraph(string valueName, Func<object> valueDelegate,
+        public GraphInfo AddGraph(string valueName, Func<object> valueDelegate,
             GameObject go) {
 
-            var valueGraph = new ValueGraph() {
+            var valueGraph = new GraphInfo() {
                 ValueName = valueName,
                 ValueDelegate = valueDelegate,
                 Go = go
             };
             _valueGraphs.Add(valueGraph);
             return valueGraph;
-        }
-    }
-
-    public class ValueGraph {
-        public string ValueName { get; set; }
-        public Func<object> ValueDelegate { get; set; } 
-        public GameObject Go { get; set; }
-        public bool Enabled { get; set; }
-
-        public GraphDrawer GraphDrawer {
-            get {
-                if (_graphDrawer == null && Go != null) {
-                    _graphDrawer = Go.GetComponent<GraphDrawer>();
-                }
-                return _graphDrawer;
-            }
-        }
-
-        private GraphDrawer _graphDrawer;
-
-        public void DrawValuePoint() {
-            var value = ValueDelegate();
-            GraphDrawer.DrawValuePoint(value);
         }
     }
 }
