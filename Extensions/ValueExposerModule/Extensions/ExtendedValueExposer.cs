25cda2d2f8c98b65bf1772e426cbfb1bdd51b749
Bartlomiej Wolk <bwolk@goodgamestudios.com>
2016-06-09 19:11:47 +0200

    Instantiate ValueVisualizer prefab

--- Extensions/ValueExposerModule/Extensions/ExtendedValueExposer.cs
+++ Extensions/ValueExposerModule/Extensions/ExtendedValueExposer.cs
@@ -11,11 +11,20 @@ namespace DevelopmentConsole.Extensions.ValueExposerModule.Extensions {
         [SerializeField]
         private ValueVisualizer _valueVisualizer;
 
+        [SerializeField]
+        private Canvas _canvas;
+
         #endregion
 
         protected override void Awake() {
             base.Awake();
             Assert.IsNotNull(_valueVisualizer);
+            Assert.IsNotNull(_canvas);
+        }
+
+        private void Start() {
+            var visualizer = Instantiate(_valueVisualizer);
+            visualizer.transform.SetParent(_canvas.transform, false);
         }
 
         public override void ShowValue(string valueName) {
